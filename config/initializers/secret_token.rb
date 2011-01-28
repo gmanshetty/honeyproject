92576951ed566ee95611e34d2cf92a204c3e3850
Gmanshetty <gmanshetty@ymail.com>
2011-01-28 01:31:55 -0500

    Gman Commitagain

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Honeyproject::Application.config.secret_token = '7bd05048b12d53330771e18d33da80cd6c5f9355062d120b3540d9cdb283437d51ce559f651d7f1824a0560eb7db512ebec13f5f4948095cd62c183c390e963e'
